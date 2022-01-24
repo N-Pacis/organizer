a4870e91f0504480c4d873dd09a44410fa534006
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-24 21:53:02 +0200

    [Feat] added backend logic for student operations

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students_list = Student::orderBy('created_at', 'desc')->get();
+        return view('dashboard.student-page',compact('students_list'));;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'firstname'=>'required',
+            'lastname'=>'required',
+            'enrolled_in'=>'required',
+            'status'=>'required'
+        ]);
+        $input = $request->all();
+        $user_id = auth()->user()->id;
+        $input['added_by'] = $user_id;
+        Student::create($input);
+        return redirect('students')->with('flash_message','Student Added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Student $student)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Student $student)
+    {
+        //
+    }
+}
---
+++ app/Models/Student.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $fillable = ['firstname','lastname','phone_number','address','enrolled_in','status','added_by'];
+    use HasFactory;
+}
