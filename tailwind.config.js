1fbd6401a650d557e2a9700ef88ad2a711e9c72f
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-25 21:40:13 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Roboto', 'Helvetica', 'sans-serif', ...defaultTheme.fontFamily.sans],
             },
         },
     },
