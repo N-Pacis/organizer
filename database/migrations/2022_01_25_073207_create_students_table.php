23591f1244b11643f539b64a051a2de19e02bd1b
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-25 21:39:35 +0200

    Create 2022_01_25_073207_create_students_table.php

---
+++ database/migrations/2022_01_25_073207_create_students_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->string('student_id');
+            $table->string('picture');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('phone_number')->nullable()->default(NULL);
+            $table->string('address')->nullable()->default(NULL);
+            $table->string('enrolled_in');
+            $table->string('status');
+            $table->bigInteger('added_by')->unsigned();
+            $table->foreign('added_by')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
